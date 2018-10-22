3d64d7b71caaab0c74c119b5e1f9762cf285ed06
Daniel Shir <daniels@viber.com>
2018-10-22 11:19:43 +0300

    Updated weather server to provide client with a way to provide the city

--- weather/server/weather.js
+++ weather/server/weather.js
@@ -1,14 +1,16 @@
 const YQL = require('yql');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const PORT = 8080;
+const DEFAULT_CITY = 'tel aviv';
 
-function fetchWeatherForTelAviv(cb) {
-    const query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="tel aviv") and u=\'c\'');
+function fetchWeatherForCity(city, cb) {
+    const query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + city + '") and u=\'c\'');
 
     query.exec((err, data) => {
         console.log("Fetched result from Yahoo");
-        
+
         if (err) return cb(err);
         if (!data.query.results) return cb(new Error("Empty results from Yahoo"));
 
@@ -30,11 +32,24 @@ function fetchWeatherForTelAviv(cb) {
 }
 
 let app = express();
+app.use(bodyParser.json());
 
 app.get('/weather', (req, res, next) => {
-    console.log("Got request to /weather");
-    fetchWeatherForTelAviv((err, weather) => {
-        if (err) return res.error(err);
+    console.log("Got GET request to /weather");
+
+    fetchWeatherForCity(DEFAULT_CITY, (err, weather) => {
+        if (err) return res.status(500).json({err: err.message});
+        return res.json(weather);
+    });
+});
+
+app.post('/weather', (req, res, next) => {
+    console.dir(req.body);
+    let city = req.body.city || DEFAULT_CITY;
+    console.log("Got POST request to /weather, using city=" + city);
+
+    fetchWeatherForCity(city, (err, weather) => {
+        if (err) return res.status(500).json({err: err.message});
         return res.json(weather);
     });
 });
