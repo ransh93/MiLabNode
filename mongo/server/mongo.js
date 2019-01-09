a6e90de5d90c8336efe15393c2ddfe5f7d1d0d04
Daniel Shir <daniels@viber.com>
2019-01-09 12:46:34 +0200

    Upgraded mongodb pacakge to version 3.x, changed logic to use promises instead

--- mongo/server/mongo.js
+++ mongo/server/mongo.js
@@ -1,38 +1,30 @@
 "use strict";
 
-const async = require('async');
 const MongoClient = require('mongodb').MongoClient;
-const MONGO_URL = "mongodb://localhost:27017/mongotest";
+const MONGO_URL = "mongodb://localhost:27017";
 
-MongoClient.connect(MONGO_URL, (err, db) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+const client = new MongoClient(MONGO_URL, {
+    useNewUrlParser: true
+});
 
+client.connect()
+.then(() => {
+    let db = client.db('mongotest');
     let collection = db.collection('docs');
-    async.series([
-        
-        cb => collection.insertMany([
-           {username: "foo"}, {username: "bar"}, {username: "something"}
-        ], cb),
-
-        cb => collection.find().toArray((err, docs) => {
-            if (err) return cb(err);
-
-            console.log(`Found ${docs.length} documents`);
-            docs.forEach(doc => console.log(doc.username));
-
-            return cb(null);
-        })
-    ], (err, result) => {
-        db.close();
-
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-        console.log("Completed successfully");
-    });
+    return collection.insertMany([
+        {username: "foo"}, {username: "bar"}, {username: "something"}
+    ]);
+})
+.then(() => {
+    let db = client.db('mongotest');
+    let collection = db.collection('docs');
+    return collection.find().toArray();
+})
+.then(docs => {
+    console.log(`Found ${docs.length} documents`);
+    docs.forEach(doc => console.log(doc.username));
+    return;
+})
+.catch(err => {
+    console.error(err);
 });
